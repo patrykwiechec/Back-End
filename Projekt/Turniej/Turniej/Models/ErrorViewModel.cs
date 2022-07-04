2bf61ffc537f02de9d872a2f1aa556f2cd22f509
patrykwiechec <80617859+patrykwiechec@users.noreply.github.com>
2022-07-04 18:37:50 +0200

    Add files via upload
    Start

---
+++ Projekt/Turniej/Turniej/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Turniej.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
