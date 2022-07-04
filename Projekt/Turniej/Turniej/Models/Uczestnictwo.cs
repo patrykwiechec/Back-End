736e53f4d8a475e06bd69ee7c75f34ce7d433b6f
Patryk Wiechec <patryk.wiechec@microsoft.wsei.edu.pl>
2022-07-04 21:33:06 +0200

    Zmiana wizualna + koniec prac

--- Projekt/Turniej/Turniej/Models/Uczestnictwo.cs
+++ Projekt/Turniej/Turniej/Models/Uczestnictwo.cs
@@ -1,12 +1,16 @@
 ﻿ using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace Turniej.Models
 {
     public partial class Uczestnictwo
     {
+        [DisplayName("ID Uczestnictwa")]
         public int IdUczestnictwa { get; set; }
+        [DisplayName("ID Zawodnika")]
         public int? IdZawodnika { get; set; }
+        [DisplayName("ID Zawodów")]
         public int? IdZawodow { get; set; }
 
         public virtual Zawodnicy? IdZawodnikaNavigation { get; set; }
